91c863ad0bb8ec0e5db0a37966288303d27ef5c6
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-11-22 11:14:53 +0100

    post route ok

--- kitten-adopt/kittenadopt/src/main/java/fr/codewise/kittenadopt/controllers/KittenController.java
+++ kitten-adopt/kittenadopt/src/main/java/fr/codewise/kittenadopt/controllers/KittenController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,10 +34,16 @@ public class KittenController {
     //adopt, getbyid, update, create
 
     //DAMIEN GET BY ID PAR ICIIIIIII
-    
+
 
 
     //****************
     //LISA CREATE KITTEN PAR LAAAAAAAA
+    @PostMapping
+    public void create(@RequestBody Kitten kitten){
+        this.kittenRepository.save(kitten);
+    }
+
+    // LISA UPDATE KITTEN PAR ICIIIIIII
     
 }
